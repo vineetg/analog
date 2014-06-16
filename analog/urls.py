856beb322db56c6b344995e23d438417f648daf9
Vineet Gupta <guptav@vmware.com>
2014-06-15 23:45:49 -0700

    Added bootstrap, custom css and first view

--- analog/urls.py
+++ analog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'analog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^document/',
+        include('document.urls', namespace='document', app_name='document')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- document/views.py
+++ document/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from document.models import Document
+
+def index(request):
+    latest_documents = Document.objects.all().order_by('-date_updated')[:5]
+    context = {'latest_documents': latest_documents}
+    return render(request, 'document/index.html', context)
+
+def create(request):
+    return render(request, 'document/index.html')
